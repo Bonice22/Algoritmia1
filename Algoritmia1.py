67a829448f879916312d324068514d61a4fe6975
Bonice 22 <nachosqueso26@gmail.com>
2025-01-28 16:21:19 -0600

    cambio

--- Algoritmia1.py
+++ Algoritmia1.py
@@ -1,4 +1,22 @@
-a = 10
-b = 193
-resultado= a + b 
-print(resultado)
+#IMC
+nombre=input("Hola, cual es tu nombre?")
+print(("hola" + nombre + "vamos a sacar tu IMC(indice de masa corporal)"))
+
+peso= float(input("para comenzar,¿cual es tu peso?(kg) "))
+altura= float(input("cual es tu altura?(m)"))
+alturaf=altura * altura
+imc= peso/alturaf
+print("tu IMC es de " , imc )
+if imc <= 18.5:
+    print("tu IMC es muy bajo ")
+elif imc >=18.5 <= 24.9:
+    print("tu IMC es normal")
+elif imc >= 24.9 <= 30:
+    print("tu IMC indica que tienes sobrepeso")
+elif imc >= 30 <= 35:
+    print("Tu IMC indica que tienes sobrepeso Grado 1")
+elif imc >= 35 <=40:
+    print("Tu IMC indica que tienes Obesidad grado 2")
+elif imc >= 40:
+    print("Tu IMC indica que tienes Obesidad grado 3(obesidad Mórbida)")
+print('muchas gracias')
---
+++ Comprobador.py
@@ -0,0 +1,5 @@
+peso= float(input("para comenzar,¿cual es tu peso?(kg) "))
+altura= float(input("cual es tu altura?(m)"))
+alturaf=altura * altura
+imc= peso/alturaf
+print("tu IMC es de " , imc )
